3e34b8bb42fa6c45ac726f2a42c2ec72edb28769
Seiji Toyama <toyama.seiji@gmail.com>
2016-09-09 12:07:19 +0900

    Fix compose link version2

--- kibana/run.sh
+++ kibana/run.sh
@@ -5,7 +5,7 @@ ES_PORT_9200_TCP_ADDR=${ES_PORT_9200_TCP_ADDR:-localhost}
 ES_PORT_9200_TCP_PORT=${ES_PORT_9200_TCP_PORT:-9200}
 
 if [ "$1" = 'kibana' ]; then
-  sed -i "s;^elasticsearch_url:.*;elasticsearch_url: http://${ES_PORT_9200_TCP_ADDR}:${ES_PORT_9200_TCP_PORT};" "/opt/kibana/config/kibana.yml"
+  # sed -i "s;^elasticsearch_url:.*;elasticsearch_url: http://${ES_PORT_9200_TCP_ADDR}:${ES_PORT_9200_TCP_PORT};" "/opt/kibana/config/kibana.yml"
   exec /opt/kibana/bin/kibana
 
 else
