abdede3734de81aeefafe2bc404a75ef94c02185
Seiji Toyama <toyama.seiji@gmail.com>
2017-03-14 21:29:00 +0900

    Bumpup redis v3.2.8

--- rpm/centos6/redis/build.sh
+++ rpm/centos6/redis/build.sh
@@ -1,16 +1,23 @@
 #!/bin/env bash
 WORKDIR=/opt/rpmbuild
 
-REDIS_VERSION=${REDIS_VERSION:-"2.8.21"}
+REDIS_VERSION=${REDIS_VERSION:-"3.2.8"}
 
 mkdir -p $WORKDIR/rpm/{BUILD,SRPMS,SPECS,SOURCES,RPMS}
 echo "%_topdir $WORKDIR/rpm" > ~/.rpmmacros
 echo "%_smp_mflags -j4" >> ~/.rpmmacros
 
+mv -i $WORKDIR/redis/redis.spec $WORKDIR/rpm/SPECS/
+mv -i $WORKDIR/redis/*.patch $WORKDIR/rpm/SOURCES/
+mv -i $WORKDIR/redis/redis-limit-init $WORKDIR/rpm/SOURCES/
+mv -i $WORKDIR/redis/redis-limit-systemd $WORKDIR/rpm/SOURCES/
+mv -i $WORKDIR/redis/redis-shutdown $WORKDIR/rpm/SOURCES/
+mv -i $WORKDIR/redis/redis-sentinel.init $WORKDIR/rpm/SOURCES/
+mv -i $WORKDIR/redis/redis-sentinel.service $WORKDIR/rpm/SOURCES/
+mv -i $WORKDIR/redis/redis-shutdown $WORKDIR/rpm/SOURCES/
 mv -i $WORKDIR/redis/redis.init $WORKDIR/rpm/SOURCES/
 mv -i $WORKDIR/redis/redis.logrotate $WORKDIR/rpm/SOURCES/
-mv -i $WORKDIR/redis/redis-2.8.21-redis.conf.patch $WORKDIR/rpm/SOURCES/
-mv -i $WORKDIR/redis/redis.spec $WORKDIR/rpm/SPECS/
+mv -i $WORKDIR/redis/redis.service $WORKDIR/rpm/SOURCES/
 
 # redis
 # rpm -ivh http://dl.fedoraproject.org/pub/epel/6/SRPMS/redis-2.4.10-1.el6.src.rpm
